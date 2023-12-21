701998b35582658979f2a33c4a29806a12e27816
Kipoha <busterx25@gmail.com>
2023-12-21 11:58:48 +0600

    hw

---
+++ game.py
@@ -0,0 +1,45 @@
+import random
+from decouple import config
+
+
+def play():
+    my_money = int(config('MY_MONEY', 1000))
+
+    while my_money > 0:
+        try:
+            print(f"Твой баланс: ${my_money}")
+            bet = int(input("Сделай ставку: "))
+
+            if my_money < bet:
+                print(f'Вам не хватает {bet - my_money}')
+            else:
+                win = random.randint(1, 10)
+
+                if int(input(f"Выберите слот (1-10): ")) == win:
+                    my_money += 2 * bet
+                    print(f"Отлично! Вы выиграли ${2 * bet}")
+                else:
+                    my_money -= bet
+                    print(f"Вы проиграли ${bet}. Ха лох!")
+
+                play_again = input("Вы хотите продолжить игру? (да/yes, если вы напишите другое, то игра остановится): ").lower()
+                if play_again == 'yes' or play_again == 'да':
+                    pass
+                else:
+                    print('Такой команды не существует')
+                    break
+        except ValueError as error:
+            print('Нужно указать цифры')
+
+
+    print(f"Игра окончена! Ваш баланс: ${my_money}")
+    if my_money == 0:
+        print('Вы проиграли всю сумму')
+    elif my_money > 1000:
+        count = my_money - 1000
+        print(f'Вы в плюсе на {count}')
+    elif my_money == 1000:
+        print('Вы ушли в 0')
+    else:
+        count = 1000 - my_money
+        print(f'Вы ушли в минус на {count}')
---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+from game import play
+
+if __name__ == "__main__":
+    play()
